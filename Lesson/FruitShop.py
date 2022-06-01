5280121e8870ba72a0978f036ba68e140e71be49
zeyi wang <wang-ze-yi@hotmail.com>
2022-05-31 23:13:50 -0400

    dictionary

---
+++ Lesson/FruitShop.py
@@ -0,0 +1,19 @@
+class FruitShop(object):
+    def __init__(self):
+        self.checklist = {}
+
+    def buyFruit(self, fruitName, amount):
+        return
+    
+    def sellFruit(self, fruitName, amount):
+        return
+    
+    def listAllFruitsSorted(self):
+        return
+    
+fruitShop = FruitShop()
+fruitShop.buyFruit("apple", 5)
+fruitShop.buyFruit("orange", 3)
+fruitShop.buyFruit("apple", 1)
+fruitShop.sellFruit("apple", 4)
+fruitShop.listAllFruitsSorted()
